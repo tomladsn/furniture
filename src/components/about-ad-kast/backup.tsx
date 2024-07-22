199515fd924fba93aed80fab313e44fdb34bec22
tomladson <108412668+tomladsn@users.noreply.github.com>
2024-07-22 15:08:24 +0100

    rese

---
+++ src/components/about-ad-kast/backup.tsx
@@ -0,0 +1,30 @@
+<div className={styles.newsletterDiv}>
+<div className={styles['newsletter-subdiv']}>
+    <div className={styles.newslettersubsubdiv}>
+        <img
+            src="/product image/Image 85deliverynewsletter.png"
+            className={styles['delivery-img']}
+        />
+        <p className={styles['delivery-head-txt']}>Snelle levering</p>
+        <p className={styles['delivery-sub-sub-txt']}>
+            Levering binnen minder dan 20 dagen
+        </p>
+        <img
+            src="/product image/Image 87newsletter.png"
+            className={styles['fast-del']}
+        />
+        <p className={styles.verdenzing}>Verzending</p>
+        <p className={styles['sub-fast-del']}>Overal in Nederland</p>
+
+        <p className={styles['newsletter-subscribe']}>
+            Inschrijven voor de nieuwsbrief
+        </p>
+        <div className={styles['field-div-newsletter']}>
+            <p className={styles['input-email']}>
+                Uw e-mail
+                <input className={styles['newsletter-input']} />
+            </p>
+        </div>
+    </div>
+</div>
+</div>
